883f1ba2cd51e03e34f89834d6bc63a7ed462ac4
ChristianAlfonsoRomeroMartinez <christian.romero-m@mail.escuelaing.edu.co>
2025-09-05 23:52:50 -0500

    Parte 2-Reto3 ChristianRomero: StringBuilder

---
+++ Reto3/Cueva.java
@@ -0,0 +1,9 @@
+public class Cueva {
+
+    //A.1 
+    public String StringBuilder(String mensaje) {
+        return java.util.stream.IntStream.range(0, 3)
+                .mapToObj(i -> mensaje)
+                .collect(java.util.stream.Collectors.joining(" "));
+    }
+}
