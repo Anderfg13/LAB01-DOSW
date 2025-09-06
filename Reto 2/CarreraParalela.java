c5c771e020a6a26a08acd115a760f93c8df58460
Anderfg13 <anderson.fgn13@gmail.com>
2025-09-05 22:53:38 -0500

    Parte 2:Reto 2 Anderson Garcia Se creo la función con el lamda minimo

--- Reto 2/CarreraParalela.java
+++ Reto 2/CarreraParalela.java
@@ -20,21 +20,11 @@ public class CarreraParalela {
 
         Function<List<Integer>, String> calcular = lista -> {
             int minimo = Collections.min(lista);
-            int maximo = Collections.max(lista);
             int cantidad = lista.size();
-            boolean isDivisible = (2 % maximo == 0) ? true : false;
-            boolean esPar = (maximo % 2 == 0) ? true : false;
-            boolean cantidadPar = (cantidad % 2 == 0) ? true : false;
-            boolean cantidadImpar = (cantidad % 2 != 0) ? true : false;
-            return "Número más pequeño: " + minimo +
-                   ", Número más grande: " + maximo +
-                   ", Cantidad de datos: " + cantidad +
-                   ", 2 es divisible por el máximo: " + isDivisible +
-                   ", Máximo es par: " + esPar +
-                   ", Cantidad es par: " + cantidadPar +
-                   ", Cantidad es impar: " + cantidadImpar;
+            return "Número más pequeño: " + minimo + ", Cantidad de datos: " + cantidad;
         };
 
-        System.out.println(calcular.apply(numeros));
+        String resultado = calcular.apply(numeros);
+        System.out.println(resultado);
     }
 }
